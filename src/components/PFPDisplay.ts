746782a3f612bff1cf013de90056a52ce5e64cd3
Sim Young Jae <hanul@hanul.me>
2022-08-10 13:49:53 +0900

    dev

--- src/components/PFPDisplay.ts
+++ src/components/PFPDisplay.ts
@@ -6,9 +6,11 @@ export default class PFPDisplay extends DomNode<HTMLVideoElement> {
         if (url !== undefined && url.indexOf(".mp4") !== -1) {
             super("video.pfp-display");
             this.domElement.src = url;
+            this.domElement.playsInline = true;
             this.domElement.defaultMuted = true;
             this.domElement.muted = true;
             this.domElement.loop = true;
+            this.domElement.autoplay = true;
             this.domElement.play();
         } else {
             super("img.pfp-display");
